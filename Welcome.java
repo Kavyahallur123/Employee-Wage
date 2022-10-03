60a3e8580899c4cd7c5a3cc94eac1b29cc38a447
kavyahallur123 <kavyahallur199@gmail.com>
2022-10-03 21:56:10 +0530

    Added:Welcome to employee wage program

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+package com.bridgelab.empwage;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("wel come to employee wage program");
+    }
+
+
+}
